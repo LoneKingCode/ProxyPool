a16a5cbbcba0de283673f58884d09a17b43874e0
LoneKingCode <lk5402@qq.com>
2024-01-19 10:49:33 +0800

    1

--- api/server.py
+++ api/server.py
@@ -19,156 +19,209 @@ class DataTableModel:
 
     def tojson(self):
         return {
-            'draw': self.draw,
-            'recordsTotal': self.recordsTotal,
-            'recordsFiltered': self.recordsFiltered,
-            'data': self.data,
+            "draw": self.draw,
+            "recordsTotal": self.recordsTotal,
+            "recordsFiltered": self.recordsFiltered,
+            "data": self.data,
         }
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/upload', methods=['POST'])
+@app.route("/upload", methods=["POST"])
 def upload():
-    if request.method == 'POST':
+    if request.method == "POST":
         # f = request.files['proxyfile']
         # f.save(upload_path)
         # 格式ip:port
-        proxies = request.form.get('proxies')
+        proxies = request.form.get("proxies")
         models = []
         if proxies:
-            proxies = proxies.split('\r\n')
+            proxies = proxies.split("\r\n")
             for p in proxies:
-                ip_port = p.split(':')
+                ip_port = p.split(":")
                 ip = SqlHelper.escape_string(ip_port[0])
                 port = SqlHelper.escape_string(ip_port[1])
-                model = {'ip': ip, 'port': port, 'speed': 0, 'score': 10, 'type': 0, 'protocol': 0, 'country': '无', 'area': '无'}
+                model = {
+                    "ip": ip,
+                    "port": port,
+                    "speed": 0,
+                    "score": 10,
+                    "type": 0,
+                    "protocol": 0,
+                    "country": "无",
+                    "area": "无",
+                }
                 models.append(model)
             row_affect = sqlhelper.add(models)
-            return json.dumps({'msg': '导入完成，导入{0}条数据'.format(row_affect)})
+            return json.dumps({"msg": "导入完成，导入{0}条数据".format(row_affect)})
         else:
-            return json.dumps({'msg': '操作失败，内容不存在'})
-    return render_template('upload.html')
+            return json.dumps({"msg": "操作失败，内容不存在"})
+    return render_template("upload.html")
 
 
-@app.route('/importdata', methods=['POST'])
+@app.route("/importdata", methods=["POST"])
 def importdata():
-    proxies = json.loads(request.data.decode('utf-8'))
+    proxies = json.loads(request.data.decode("utf-8"))
     proxies = json.loads(proxies)
     # 格式 ip:port
     models = []
     if proxies:
         for p in proxies:
-            ip_port = p.split(':')
+            ip_port = p.split(":")
             ip = SqlHelper.escape_string(ip_port[0])
             port = SqlHelper.escape_string(ip_port[1])
-            model = {'ip': ip, 'port': port, 'speed': 0, 'score': 10, 'type': 0, 'protocol': 0, 'country': '无', 'area': '无'}
+            model = {
+                "ip": ip,
+                "port": port,
+                "speed": 0,
+                "score": 10,
+                "type": 0,
+                "protocol": 0,
+                "country": "无",
+                "area": "无",
+            }
             models.append(model)
         row_affect = sqlhelper.add(models)
-        return json.dumps({'msg': '导入完成，导入{0}条数据'.format(row_affect)})
+        return json.dumps({"msg": "导入完成，导入{0}条数据".format(row_affect)})
 
     else:
-        return json.dumps({'msg': '操作失败，内容不存在'})
+        return json.dumps({"msg": "操作失败，内容不存在"})
 
 
-@app.route('/count', methods=['POST', 'GET'])
+@app.route("/count", methods=["POST", "GET"])
 def count():
     result = []
     try:
         # /?count=5&condition=speed<3 自定义where条件
         # /?count=5&country=国外 只能输入=的条件
         # / 获取所有记录
-        condition = SqlHelper.escape_string(request.args.get('condition', ''))
+        condition = SqlHelper.escape_string(request.args.get("condition", ""))
         if condition:
-            result = sqlhelper.query('select * from proxy_main where %s' % condition)
+            result = sqlhelper.query("select * from proxy_main where %s" % condition)
         else:
             result = sqlhelper.get(request.args)
     except:
-        return 'error'
+        return "error"
     return str(len(result))
 
 
-@app.route('/get', methods=['POST', 'GET'])
+@app.route("/get", methods=["POST", "GET"])
 def get():
     result = []
     try:
         # /?count=5&condition=speed<3 自定义where条件
         # /?count=5&country=国外 只能输入=的条件
         # / 获取所有记录
-        condition = request.args.get('condition')
-        count = request.args.get('count')
+        condition = request.args.get("condition", "")
+        count = request.args.get("count", "0")
         if condition:
-            result = sqlhelper.query('select * from proxy_main where %s' % condition, count)
+            result = sqlhelper.query(
+                "select * from proxy_main where %s" % condition, count
+            )
         else:
             result = sqlhelper.get(request.args, count)
     except:
-        return 'error'
+        return "error"
 
-    list_result = '<br/>'.join(["{0}:{1}".format(r['ip'], r['port']) for r in result])
+    list_result = "<br/>".join(["{0}:{1}".format(r["ip"], r["port"]) for r in result])
     return list_result
 
 
-@app.route('/get_format', methods=['POST', 'GET'])
+@app.route("/get_format", methods=["POST", "GET"])
 def get_format():
     result = []
     try:
         # /?count=5&condition=speed<3 自定义where条件
         # /?count=5&country=国外 只能输入=的条件
         # / 获取所有记录
-        condition = request.args.get('condition')
-        count = request.args.get('count')
+        condition = request.args.get("condition")
+        count = request.args.get("count")
         if condition:
-            result = sqlhelper.query('select * from proxy_main where %s' % condition, count)
+            result = sqlhelper.query(
+                "select * from proxy_main where %s" % condition, count
+            )
         else:
             result = sqlhelper.get(request.args, count)
     except:
-        return 'error'
-    list_result = '<br/>'.join(["{0}://{1}:{2}".format(('http' if r['type'] == ProxyProtocol.http.value else 'https'), r['ip'], r['port']) for r in result])
+        return "error"
+    list_result = "<br/>".join(
+        [
+            "{0}://{1}:{2}".format(
+                ("http" if r["type"] == ProxyProtocol.http.value else "https"),
+                r["ip"],
+                r["port"],
+            )
+            for r in result
+        ]
+    )
     return list_result
 
 
-@app.route('/get_pagedata', methods=['POST', 'GET'])
+@app.route("/get_pagedata", methods=["POST", "GET"])
 def get_pagedata():
     result = []
     try:
-        searchkey = SqlHelper.escape_string(request.values.get('searchKey', ''))
-        orderby = SqlHelper.escape_string(request.values.get('orderBy', 'score'))
-        draw = SqlHelper.escape_string(request.values.get('draw', ''))
-        orderdir = SqlHelper.escape_string(request.values.get('orderDir', 'desc'))  # asc or desc
-        start = SqlHelper.escape_string(request.values.get('start', 0))
-        length = SqlHelper.escape_string(request.values.get('length', 10))
-        sql = 'select {0} from {1} where {2} order by {3} limit {4},{5}'
-        result = sqlhelper.query(sql.format('*', 'proxy_main', 'country like \'%{0}%\' or area like \'%{0}%\''.format(searchkey), orderby + ' ' + orderdir, start, length))
-        totalcount = sqlhelper.query('select {0} from {1} where {2}'.format('count(*)', 'proxy_main', 'country like \'%{0}%\' or area like \'%{0}%\''.format(searchkey)))[0]['count(*)']
+        searchkey = SqlHelper.escape_string(request.values.get("searchKey", ""))
+        orderby = SqlHelper.escape_string(request.values.get("orderBy", "score"))
+        draw = SqlHelper.escape_string(request.values.get("draw", ""))
+        orderdir = SqlHelper.escape_string(
+            request.values.get("orderDir", "desc")
+        )  # asc or desc
+        start = SqlHelper.escape_string(request.values.get("start", 0))
+        length = SqlHelper.escape_string(request.values.get("length", 10))
+        sql = "select {0} from {1} where {2} order by {3} limit {4},{5}"
+        result = sqlhelper.query(
+            sql.format(
+                "*",
+                "proxy_main",
+                "country like '%{0}%' or area like '%{0}%'".format(searchkey),
+                orderby + " " + orderdir,
+                start,
+                length,
+            )
+        )
+        totalcount = sqlhelper.query(
+            "select {0} from {1} where {2}".format(
+                "count(*)",
+                "proxy_main",
+                "country like '%{0}%' or area like '%{0}%'".format(searchkey),
+            )
+        )[0]["count(*)"]
     except:
-        return 'error'
+        return "error"
     count = 0
     for row in result:
         for key, value in row.items():
-            if key == 'type':
+            if key == "type":
                 result[count][key] = ProxyType(int(value)).name
-            elif key == 'protocol':
+            elif key == "protocol":
                 result[count][key] = ProxyProtocol(int(value)).name
         count = count + 1
-    json_result = json.dumps(DataTableModel(draw, totalcount, totalcount, result).tojson())
+    json_result = json.dumps(
+        DataTableModel(draw, totalcount, totalcount, result).tojson()
+    )
     return json_result
 
 
-@app.route('/delete', methods=['POST', 'GET'])
+@app.route("/delete", methods=["POST", "GET"])
 def delete():
     # /?count=5&country=国外 只能输入=的条件
     params = request.args
     row_affect = json.dumps(sqlhelper.delete(params))
-    return json.dumps({'msg': '操作完成，删除{0}条数据'.format(row_affect)})
+    return json.dumps({"msg": "操作完成，删除{0}条数据".format(row_affect)})
 
 
 def start_api_server():
-    print('>>>API服务器开启成功,访问地址http://%s:%s' % (config.API_SERVER_IP, config.API_SERVER_PORT))
+    print(
+        ">>>API服务器开启成功,访问地址http://%s:%s"
+        % (config.API_SERVER_IP, config.API_SERVER_PORT)
+    )
     app.run(host=config.API_SERVER_IP, port=config.API_SERVER_PORT, debug=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     start_api_server()
--- db/sqlhelper.py
+++ db/sqlhelper.py
@@ -5,7 +5,7 @@ import pymysql
 from config import DATABASE_CONFIG
 from util.loghelper import LogHelper
 
-allow_columns = ['ip', 'port', 'type', 'protocol', 'country', 'area', 'score', 'speed']
+allow_columns = ["ip", "port", "type", "protocol", "country", "area", "score", "speed"]
 
 # 数据库中列 id ip port speed type protocol country area score checkdatetime
 
@@ -24,14 +24,14 @@ class SqlHelper(object):
     @staticmethod
     def escape_string(value):
         value = str(value)
-        value = value.replace('\\', '\\\\')
-        value = value.replace('\0', '\\0')
-        value = value.replace('\n', '\\n')
-        value = value.replace('\r', '\\r')
-        value = value.replace('\032', '\\Z')
+        value = value.replace("\\", "\\\\")
+        value = value.replace("\0", "\\0")
+        value = value.replace("\n", "\\n")
+        value = value.replace("\r", "\\r")
+        value = value.replace("\032", "\\Z")
         value = value.replace("'", "\\'")
         value = value.replace('"', '\\"')
-        value = value.replace(',', ',')
+        value = value.replace(",", ",")
         return value
 
     # def __init__(self):
@@ -46,18 +46,28 @@ class SqlHelper(object):
 
     def __get_conn(self):
         try:
-            self.conn = pymysql.connect(host=DATABASE_CONFIG['ip'], port=DATABASE_CONFIG['port'], \
-                                        user=DATABASE_CONFIG['username'], passwd=DATABASE_CONFIG['password'],
-                                        db=DATABASE_CONFIG['database'], connect_timeout=100)
+            self.conn = pymysql.connect(
+                host=DATABASE_CONFIG["ip"],
+                port=DATABASE_CONFIG["port"],
+                user=DATABASE_CONFIG["username"],
+                passwd=DATABASE_CONFIG["password"],
+                db=DATABASE_CONFIG["database"],
+                connect_timeout=100,
+            )
             return self.conn
         except Exception as e:
             try:
-                self.conn = pymysql.connect(host=DATABASE_CONFIG['ip'], port=DATABASE_CONFIG['port'], \
-                                            user=DATABASE_CONFIG['username'], passwd=DATABASE_CONFIG['password'],
-                                            db=DATABASE_CONFIG['database'], connect_timeout=100)
+                self.conn = pymysql.connect(
+                    host=DATABASE_CONFIG["ip"],
+                    port=DATABASE_CONFIG["port"],
+                    user=DATABASE_CONFIG["username"],
+                    passwd=DATABASE_CONFIG["password"],
+                    db=DATABASE_CONFIG["database"],
+                    connect_timeout=100,
+                )
                 return self.conn
             except Exception as e:
-                LogHelper.error('获取数据库连接出错:' + str(e))
+                LogHelper.error("获取数据库连接出错:" + str(e))
 
     def execute(self, sql):
         return self.execute_many([sql])
@@ -73,38 +83,40 @@ class SqlHelper(object):
                 conn.commit()
             except Exception as e:
                 conn.rollback()
-                LogHelper.error('执行{0}出错,错误原因:{1}'.format(sqls, str(e)))
+                LogHelper.error("执行{0}出错,错误原因:{1}".format(sqls, str(e)))
             finally:
                 self.__close(cursor, conn)
         except Exception as e:
-            LogHelper.error('获取数据库连接出错,语句{0},错误原因:{1}'.format(','.join(sqls), str(e)))
+            LogHelper.error("获取数据库连接出错,语句{0},错误原因:{1}".format(",".join(sqls), str(e)))
         return effect_row
 
     # 去重复
     def deduplication(self):
-        self.execute('DELETE proxy_main  \
+        self.execute(
+            "DELETE proxy_main  \
         FROM \
 	        proxy_main, \
 	        ( SELECT min( id ) id, ip, `port` FROM proxy_main GROUP BY ip, `port` HAVING count( * ) > 1 ) t2  \
         WHERE  \
 	        proxy_main.ip = t2.ip  \
 	        AND proxy_main.`port` = t2.`port`  \
-	        AND proxy_main.id > t2.id;')
+	        AND proxy_main.id > t2.id;"
+        )
 
     def removeInvalidProxy(self):
-        self.execute('delete from proxy_main where score < 5')
+        self.execute("delete from proxy_main where score < 5")
 
     def query(self, sql, count=0):
         conn = self.__get_conn()
         cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
         result = None
         try:
-            if count and count > 0:
-                sql += ' limit ' + count
+            if count and int(count) > 0:
+                sql += " limit " + count
             cursor.execute(sql)
             result = cursor.fetchall()
         except Exception as e:
-            LogHelper.error('执行{0}出错,错误原因:{1}'.format(sql, str(e)))
+            LogHelper.error("执行{0}出错,错误原因:{1}".format(sql, str(e)))
         finally:
             self.__close(cursor, conn)
         return result
@@ -113,72 +125,95 @@ class SqlHelper(object):
         effect_row = 0
         # 如果添加的是单个
         if isinstance(model, dict):
-            sql = 'insert into proxy_main({0}) values({1})'
-            columns = ','.join(list(model.keys()))
-            values = "'" + ','.join([str(v) for v in model.values()]).replace(',', "','") + "'"
+            sql = "insert into proxy_main({0}) values({1})"
+            columns = ",".join(list(model.keys()))
+            values = (
+                "'"
+                + ",".join([str(v) for v in model.values()]).replace(",", "','")
+                + "'"
+            )
             effect_row = self.execute(sql.format(columns, values))
         # 添加的多个
         elif isinstance(model, list):
             sqllist = []
-            sql = 'insert into proxy_main({0}) values({1})'
+            sql = "insert into proxy_main({0}) values({1})"
             for item in model:
-                columns = ','.join(list(item.keys()))
-                values = "'" + ','.join([str(v) for v in item.values()]).replace(',', "','") + "'"
+                columns = ",".join(list(item.keys()))
+                values = (
+                    "'"
+                    + ",".join([str(v) for v in item.values()]).replace(",", "','")
+                    + "'"
+                )
                 sqllist.append(sql.format(columns, values))
             effect_row = self.execute_many(sqllist)
         return effect_row
 
     def get(self, conditions=None, count=0):
-        where = '1=1'
+        where = "1=1"
         if conditions and isinstance(conditions, dict):
-            where = ''
+            where = ""
             for key, value in conditions.items():
                 if key in allow_columns:
-                    where += key + '=' + '\'' + SqlHelper.escape_string(value) + '\'' + ' and '
-            where = where.strip('and ')
-            if where == '':
-                where = '1=1'
+                    where += (
+                        key + "=" + "'" + SqlHelper.escape_string(value) + "'" + " and "
+                    )
+            where = where.strip("and ")
+            if where == "":
+                where = "1=1"
         if count == 0 or not count:
-            result = self.query('select * from proxy_main where ' + where)
+            result = self.query("select * from proxy_main where " + where)
         else:
-            result = self.query('select * from proxy_main where ' + where, count)
+            result = self.query("select * from proxy_main where " + where, count)
         return result
 
-    def get_valid_proxy(self, count=0, country=''):
-        where = 'score > 10'
+    def get_valid_proxy(self, count=0, country=""):
+        where = "score > 10"
         if country:
             where += " and country = '{}'".format(country)
         if count == 0 or not count:
-            result = self.query("select * from proxy_main where {} order by score desc".format(where))
+            result = self.query(
+                "select * from proxy_main where {} order by score desc".format(where)
+            )
         else:
-            result = self.query("select * from proxy_main where {} order by score desc limit {}".format(where, count))
+            result = self.query(
+                "select * from proxy_main where {} order by score desc limit {}".format(
+                    where, count
+                )
+            )
         return result
 
     def update(self, model, conditions):
-        where = ''
+        where = ""
         for key, value in conditions.items():
-            where += key + '=' + '\'' + str(value) + '\'' + ' and '
-        where = where.strip('and ')
-        values = ''
+            where += key + "=" + "'" + str(value) + "'" + " and "
+        where = where.strip("and ")
+        values = ""
         for key, value in model.items():
-            values += '{0}=\'{1}\','.format(key, SqlHelper.escape_string(str(value)))
-        values = values.strip(',')
-        effect_row = self.execute('update proxy_main set ' + values + ' where ' + where)
+            values += "{0}='{1}',".format(key, SqlHelper.escape_string(str(value)))
+        values = values.strip(",")
+        effect_row = self.execute("update proxy_main set " + values + " where " + where)
         return effect_row
 
     def delete(self, conditions):
-        where = ''
+        where = ""
         if not isinstance(conditions, dict) or not conditions:
             return -1
         for key, value in conditions.items():
             if key in allow_columns:
-                where += key + '=' + '\'' + SqlHelper.escape_string(str(value)) + '\'' + ' and '
-        where = where.strip('and ')
-        effect_row = self.execute('delete from proxy_main where ' + where)
+                where += (
+                    key
+                    + "="
+                    + "'"
+                    + SqlHelper.escape_string(str(value))
+                    + "'"
+                    + " and "
+                )
+        where = where.strip("and ")
+        effect_row = self.execute("delete from proxy_main where " + where)
         return effect_row
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sql = SqlHelper()
 
     # a =
